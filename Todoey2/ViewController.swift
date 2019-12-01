1afaaa62df71ba6ad197921ae63290e7cec6fd56
Artie Ford <adf.ford@gmail.com>
2019-12-01 07:48:50 -0500

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by Arthur Ford on 12/1/19.
+//  Copyright © 2019 Arthur Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

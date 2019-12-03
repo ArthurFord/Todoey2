875dae98bba2255771a6a40cde7a36d899c40a21
Artie Ford <adf.ford@gmail.com>
2019-12-03 06:40:44 -0500

    enabled CoreData, categories, and relationships

---
+++ Todoey2/Controllers/CategoryViewController.swift
@@ -0,0 +1,115 @@
+//
+//  CategoryViewController.swift
+//  Todoey2
+//
+//  Created by Arthur Ford on 12/2/19.
+//  Copyright © 2019 Arthur Ford. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categories: [Category] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryItemCell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
+    
+    //MARK:- TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ToDoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.title = destinationVC.selectedCategory?.name
+        }
+    }
+    
+    //MARK:- Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            //what will happen when user clicks add itme button on the alert
+            if textField.text != "" {
+                
+                let newCategory = Category(context: self.context)
+                newCategory.name = textField.text!
+                
+                self.categories.append(newCategory)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                self.saveCategories()
+            }
+            
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: - Model manipulation methods
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            let result = try context.fetch(request)
+            categories = result
+        } catch {
+            print("Broken load \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+}
